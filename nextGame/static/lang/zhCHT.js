a4e64100b3a6f86fb483e8e850f6959d1cbb80a9
timenftbsc <116679934+timenftbsc@users.noreply.github.com>
2022-12-15 11:08:54 +0800

    Add files via upload

---
+++ nextGame/static/lang/zhCHT.js
@@ -0,0 +1,13 @@
+module.exports = {
+    message: {
+        hello: '妳好',
+        about: '關於',
+        welcome: "歡迎",
+        theTotal: "本轮共",
+        overallPledge: "总体质押(LP)",
+        myReward: "我的奖励(DAOS)",
+        myDAOS: "我的DAOS",
+        myPledge: "我的质押(LP)",
+        extractReward: "提取奖励"
+    }
+};
